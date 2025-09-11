7970069bc7603091e76741b418ecba6b51010e1d
Feyza Yarcı <yarcifeyza@gmail.com>
2025-09-11 05:14:24 +0300

    edited

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from '../../redux/auth/operations';
 import { clearContacts } from '../../redux/contacts/slice';
 import { useNavigate } from 'react-router-dom';
+import styles from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -15,9 +16,9 @@ const UserMenu = () => {
   };
 
   return (
-    <div>
-      <span>{user.name}</span>
-      <button onClick={handleLogout}>Logout</button>
+    <div  className={styles.menuRight}>
+        <span>Hoşgeldin, {user.name}!</span>
+      <button className={styles.logoutBtn} onClick={handleLogout}>Logout</button>
     </div>
   );
 };
